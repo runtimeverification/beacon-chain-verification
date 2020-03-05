8fadd4db7f4bdc0164cbe4938308baf75017c5ca
Musab Alturki <42160792+malturki@users.noreply.github.com>
2020-03-05 16:01:41 -0600

    Generalized safety theorem to k-finalization  (#24)
    * completed same-k proofs modulo ssrnat reasoning
    
    * different k safety proofs complete
    
    * removed redundant definitions and proofs, proved equivalence to previous definitions

--- casper/coq/theories/Justification.v
+++ casper/coq/theories/Justification.v
@@ -3,6 +3,7 @@ From mathcomp.ssreflect
 Require Import all_ssreflect.
 Set Warnings "parsing".
 
+Require Import Nat. 
 From mathcomp.finmap
 Require Import finmap.
 
@@ -12,6 +13,14 @@ Require Import Quorums HashTree State.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
+
+Tactic Notation "spec" hyp(H) :=
+  match type of H with ?a -> _ => 
+    let H1 := fresh in (assert (H1: a); [|generalize (H H1); clear H H1; intro H]) end.
+Tactic Notation "spec" hyp(H) constr(a) :=
+  (generalize (H a); clear H; intro H). 
+Tactic Notation "spec" hyp(H) constr(a) constr(b) :=
+  (generalize (H a b); clear H; intro H).
 
 (* Definitions of justification and finalization *)
 
@@ -23,7 +32,7 @@ Definition link_supporters st s t s_h t_h : {set Validator} :=
 Definition supermajority_link (st:State) (s t : Hash) (s_h t_h : nat) : bool :=
   link_supporters st s t s_h t_h \in quorum_2.
 
-(* Adding more votes to a state preserves supermajority links *)
+(* Adding more votes (from the same validators) to a state preserves supermajority links *)
 Lemma supermajority_weaken: forall (st st':State)
   (HSub:forall (v: Vote), v \in st -> v \in st'),
     forall s t s_h t_h,
@@ -73,6 +82,87 @@ Definition finalized st b b_h :=
   justified st b b_h /\
   exists c, (b <~ c /\ supermajority_link st b c b_h b_h.+1).
 
+(* A k-finalized block is a justified block that has a k-descendent who is also justified by a supermajority link to the block, and all blocks to the descendent are also justified *)
+Definition k_finalized st b b_h k :=
+  k >= 1 /\ 
+  exists ls, size ls = k.+1 /\
+        head b ls = b /\
+        (forall n, n <= k ->
+              justified st (nth b ls n) (b_h+n) /\
+              nth_ancestor n b (nth b ls n)
+        ) /\
+        supermajority_link st b (last b ls) b_h (b_h+k). 
+
+Lemma leq_one_means_zero_or_one : forall n,
+    n <= 1 -> n = 0 \/ n = 1. 
+Proof.
+  intros n H_leq.
+  induction n. left; reflexivity.
+  spec IHn. intuition. destruct IHn. right.
+  subst. easy. subst. inversion H_leq.
+Qed.
+
+(* 1-finalized <-> k-finalized *)
+Lemma finalized_means_one_finalized : forall st b b_h,
+    finalized st b b_h <->
+    k_finalized st b b_h 1. 
+Proof.
+  intros st b b_h.
+  split; intro Hfin. 
+  (* -> *)
+  destruct Hfin as [Hjust [c [H_rel H_link]]]. 
+  split. 
+  easy.
+  exists [::b;c].  
+  repeat split.
+  apply leq_one_means_zero_or_one in H.
+  destruct H as [H_zero | H_one]; subst. 
+  simpl. rewrite <- plus_n_O. exact Hjust. 
+  apply justified_link with b b_h. exact Hjust.
+  repeat split. rewrite <- addn1.
+  easy. rewrite Minus.minus_plus.
+  apply parent_ancestor. assumption.
+  rewrite PeanoNat.Nat.add_1_r. exact H_link.
+  apply leq_one_means_zero_or_one in H.
+  destruct H as [H_zero | H_one]; subst. 
+  simpl. apply nth_ancestor_0.
+  simpl. apply parent_ancestor. exact H_rel.
+  simpl. rewrite PeanoNat.Nat.add_1_r. exact H_link.
+  (* <- *)
+  destruct Hfin as [H_k [ls [H_size [H_hd [H_rel H_link]]]]].
+  split. spec H_rel 0. spec H_rel. easy. destruct H_rel as [H_just H_an].
+  rewrite <- nth0 in H_hd.
+  rewrite H_hd in H_just.
+  rewrite PeanoNat.Nat.add_0_r in H_just.
+  exact H_just.
+  spec H_rel 1. 
+  spec H_rel. easy.
+  destruct H_rel as [H_just H_an].
+  exists (nth b ls 1). split.
+  inversion H_an; subst.
+  apply parent_ancestor. exact H_an. 
+  rewrite <- nth_last in H_link. 
+  rewrite H_size in H_link.
+  replace (2.-1) with 1 in H_link by auto.
+  rewrite PeanoNat.Nat.add_1_r in H_link.
+  assumption.
+Qed. 
+
+(* A k-finalized block is justified *)
+Lemma k_finalized_means_justified: forall st b b_h k,
+    k_finalized st b b_h k ->
+    justified st b b_h. 
+Proof. 
+  intros st b b_h k [H_k [ls [H_size [H_hd [H_rel H_link]]]]].
+  spec H_rel 0. spec H_rel.
+  easy. destruct H_rel.
+  rewrite <- nth0 in H_hd.
+  rewrite H_hd in H.
+  replace (b_h + 0) with b_h in H.
+  assumption.
+  rewrite PeanoNat.Nat.add_0_r. reflexivity.
+Qed. 
+
 (* A finalized block has a child who is justified *)
 Lemma finalized_means_justified_child: forall st p p_h,
   finalized st p p_h -> exists c, p <~ c /\ justified st c p_h.+1.
@@ -82,9 +172,14 @@ unfold finalized in Hfin.
 destruct Hfin as [Hjustified_p Hchild].
 destruct Hchild as [c [Hc_parent Hc_sm]].
 exists c. split. assumption.
-have Hp := parent_ancestor Hc_parent.
+assert (Hpc := parent_ancestor p c). 
+destruct Hpc as [Hp _].
+spec Hp Hc_parent. 
 have Hc_h : p_h.+1 > p_h by trivial.
-replace 1 with (p_h.+1 - p_h) in Hp by (rewrite subSnn;reflexivity).
-have Hjl : justification_link st p c p_h p_h.+1 by trivial.
+have Hjl : justification_link st p c p_h p_h.+1. split.
+assumption. split. 2: assumption.
+rewrite <- Minus.minus_Sn_m.
+rewrite PeanoNat.Nat.sub_diag. assumption.
+by trivial.
 apply (justified_link Hjustified_p Hjl).
 Qed.
